af63222d63943fa894274e353d62961350029d47
whhjdi <418605872@qq.com>
2018-07-02 12:28:02 +0800

    功能基本实现

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="main">
+      <router-view name="slidebar"></router-view>
+      <router-view name="main"></router-view>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header'
+export default {
+  name: 'App',
+    components:{
+    Header,
+  }
+}
+</script>
+
+<style>
+/* body{
+  background: #e1e1e1;
+} */
+.main{
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="header clearfix">
+      <a href="#">
+        <img src="../assets/cnodejs_light.svg" alt="logo">
+      </a>
+      <div class="wrapper">
+        <ul class="clearfix">
+          <li><a href="">首页</a></li>
+          <li><a href="">新手入门</a></li>
+          <li><a href="">API</a></li>
+          <li><a href="">关于</a></li>
+          <li><a href="">注册</a></li>
+          <li><a href="">登录</a></li>
+        </ul>
+      </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  .clearfix::after{
+    content: '';
+    display: block;
+    clear: both;
+  }
+  .header{
+    background-color:#5a5555;
+    height: 50px;
+  }
+  .header>a{
+    display: block;
+    float: left;
+    width: 120px;
+    height: 34px;
+    line-height: 34px;
+    margin-left: 50px;
+    margin-top:8px;
+  }
+  .wrapper{
+    float: right;
+  }
+
+  ul{
+    list-style: none;
+  }
+  li{
+    float: left;
+    padding: 15px 15px ;
+  }
+  a{
+    text-decoration: none;
+    color: #ccc;
+    font-size: 13px;
+    line-height: 20px;
+
+  }
+</style>
